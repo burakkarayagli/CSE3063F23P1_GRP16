ec543997bb5bf2c89ca496e3841c5560ad0f36fd
Efe Özgen <efe.ozgennn@gmail.com>
2023-11-14 20:39:23 +0300

    transcript ve grade done

---
+++ src/Transcript.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Transcript {
+    private ArrayList<Grade> listGrades;
+
+    public Transcript() {
+        listGrades = new ArrayList<Grade>();
+    }
+
+    public Transcript(ArrayList<Grade> initialGrades) {
+        listGrades = new ArrayList<Grade>(initialGrades);
+    }
+
+    public String getGrades() {
+        StringBuilder result = new StringBuilder();
+        for (Grade grade : listGrades) {
+            result.append(grade.getCourse().toString()).append(": ").append(grade.getGrade()).append("\n");
+        }
+        return result.toString();
+    }
+
+    public boolean addGrade(Grade grade) {
+        listGrades.add(grade);
+        return (grade == null ? true : false);
+    }
+
+    public boolean deleteGrade(Grade grade) {
+        for(int i = 0; i < listGrades.size(); i++) {
+            if(listGrades.get(i) == grade) {
+                listGrades.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
