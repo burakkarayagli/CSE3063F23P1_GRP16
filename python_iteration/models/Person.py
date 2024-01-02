b485e0c6ae76848516509c33677057c7849357d3
barisgirayakman <girayakman@gmail.com>
2024-01-02 20:52:21 +0300

    adjusted private fields

--- python_iteration/models/Person.py
+++ python_iteration/models/Person.py
@@ -1,37 +1,38 @@
-
+from abc import ABC, abstractmethod
 class Person:    
     def __init__(self, personName, personSurname, username, password):
-        self.personName = personName
-        self.personSurname = personSurname
-        self.username = username
-        self.password = password
+        self.__personName = personName
+        self.__personSurname = personSurname
+        self.__username = username
+        self.__password = password
     
     
     def getPersonName(self):
-        return self.personName
+        return self.__personName
     
     def getFullName(self):
-        return self.personName+" "+self.personSurname
+        return self.__personName+" "+self.__personSurname
     
     def setPersonName(self, personName):
-        self.personName = personName
+        self.__personName = personName
     
     def getPersonSurname(self):
-        return self.personSurname
+        return self.__personSurname
 
     def setPersonSurname(self, personSurname):
-        self.personName = personName
+        self.__personName = personName
 
     
     def getUsername(self):
-        return self.username
+        return self.__username
 
     def setUsername(self, username):
-        self.username = username
+        self.__username = username
     
     def setPassword(self, password):
-        self.password = password
+        self.__password = password
 
     # There must be an abstract method for menu
+    @abstractmethod
     def getMenu(self):
         pass
