64aaaa58c8d6d875d7d73338ef91ad9c997ddeee
atilla544 <atillagel54@hotmail.com>
2024-01-04 21:53:21 +0300

    transcript and grade python added

--- python_iteration/models/Grade.py
+++ python_iteration/models/Grade.py
@@ -1,7 +1,6 @@
-class Course:
-    def __init__(self, course_name=None, instructor=None):
-        self.course_name = course_name
-        self.instructor = instructor
+from Student import Student
+from Transcript import Transcript
+from Course import Course
 
 class Grade:
     def __init__(self, course=None, grade=None):
@@ -26,30 +25,3 @@ class Grade:
     @grade.setter
     def grade(self, grade):
         self._grade = grade
-
-class Transcript:
-    def __init__(self):
-        self.__passed_courses = []
-        self.__taken_courses = []
-
-    def add_passed_course(self, course):
-        self.__passed_courses.append(course)
-
-    def add_taken_course(self, course):
-        self.__taken_courses.append(course)
-
-    def get_passed_courses(self):
-        return self.__passed_courses
-
-    def get_taken_courses(self):
-        return self.__taken_courses
-
-class Student:
-    def __init__(self, transcript):
-        self.__transcript = transcript
-
-    def get_passed_courses(self):
-        return self.__transcript.get_passed_courses()
-
-    def get_taken_courses(self):
-        return self.__transcript.get_taken_courses()
